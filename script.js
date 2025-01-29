889a96eb81cd2d1da4b3f79a55731c4606ecd29a
DfrancoGit <ddelport101@gmail.com>
2025-01-29 14:58:01 +0200

    post

---
+++ script.js
@@ -0,0 +1,222 @@
+// ICON MAP
+
+const ICON_MAP = new Map();
+
+addMapping([0, 1], "sun");
+addMapping([2], "cloud-sun");
+addMapping([3], "cloud");
+addMapping([45, 48], "smog");
+addMapping([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82], "cloud-showers-heavy");
+addMapping([71, 73, 75, 77, 85, 86], "snowflake");
+addMapping([95, 96, 99], "cloud-bolt");
+
+function addMapping(values, icon) {
+  values.forEach((value) => {
+    ICON_MAP.set(value, icon);
+  });
+}
+
+// WEATHER
+
+function getWeather(lat, lon, timezone) {
+  const baseUrl = "https://api.open-meteo.com/v1/forecast";
+  const queryString = `?latitude=${lat}&longitude=${lon}&timezone=${timezone}&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime`;
+  return fetch(baseUrl + queryString)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return {
+        current: parseCurrentWeather(data),
+        daily: parseDailyWeather(data),
+        hourly: parseHourlyWeather(data),
+      };
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+function parseCurrentWeather({ current, daily }) {
+  const {
+    temperature_2m: currentTemp,
+    wind_speed_10m: windSpeed,
+    weather_code: iconCode,
+  } = current;
+  const {
+    temperature_2m_max: [maxTemp],
+    temperature_2m_min: [minTemp],
+    apparent_temperature_max: [maxFeelsLike],
+    apparent_temperature_min: [minFeelsLike],
+    precipitation_sum: [precip],
+  } = daily;
+
+  return {
+    currentTemp: Math.round(currentTemp),
+    highTemp: Math.round(maxTemp),
+    lowTemp: Math.round(minTemp),
+    highFeelsLike: Math.round(maxFeelsLike),
+    lowFeelsLike: Math.round(minFeelsLike),
+    windSpeed: Math.round(windSpeed),
+    precip: Math.round(precip * 100) / 100,
+    iconCode,
+  };
+}
+
+function parseDailyWeather({ daily }) {
+  return daily.time.map((time, index) => {
+    return {
+      timestamp: time * 1000,
+      iconCode: daily.weather_code[index],
+      maxTemp: Math.round(daily.temperature_2m_max[index]),
+    };
+  });
+}
+
+function parseHourlyWeather({ hourly, current }) {
+  return hourly.time
+    .map((time, index) => {
+      return {
+        timestamp: time * 1000,
+        iconCode: hourly.weather_code[index],
+        temp: Math.round(hourly.temperature_2m[index]),
+        feelsLike: Math.round(hourly.apparent_temperature[index]),
+        windSpeed: Math.round(hourly.wind_speed_10m[index] * 100) / 100,
+        precip: hourly.precipitation[index] || 0,
+      };
+    })
+    .filter(({ timestamp }) => timestamp >= current.time * 1000);
+}
+
+// MAIN
+
+const headerSection = document.getElementById("header-section");
+const daySection = document.getElementById("day-section");
+const tableSection = document.getElementById("table-section");
+
+// Geocoding
+
+const geocodingApiKey = "e2c698c42e064432ae0ae88303b47af0";
+
+document.getElementById("searchButton").addEventListener("click", convertWeatherData);
+
+function convertWeatherData() {
+  const location = document.getElementById("locationInput").value;
+
+  if (location) {
+    fetch(
+      `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+        location
+      )}&key=${geocodingApiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const lat = data.results[0].geometry.lat;
+        const lon = data.results[0].geometry.lng;
+        const timezoneResult = data.results[0].annotations.timezone.name;
+        getWeather(lat, lon, timezoneResult)
+          .then(renderWeather)
+          .catch((e) => console.error(e));
+      });
+  }
+}
+
+// Get weather
+
+// getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+//   .then(renderWeather)
+//   .catch((e) => {
+//     console.error(e);
+//     // alert("Error getting weather.");
+//   });
+
+function renderWeather({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  renderDailyWeather(daily);
+  renderHourlyWeather(hourly);
+
+  headerSection.classList.remove("blurred");
+  daySection.classList.remove("blurred");
+  tableSection.classList.remove("blurred");
+
+  document.getElementById("heading-name").innerHTML = `${
+    document.getElementById("locationInput").value
+  }`;
+  document.getElementById("locationInput").value = "";
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value;
+}
+
+function getIconUrl(iconCode) {
+  return `./icons/${ICON_MAP.get(iconCode)}.svg`;
+}
+
+const currentIcon = document.querySelector("[data-current-icon]");
+
+function renderCurrentWeather(current) {
+  currentIcon.src = getIconUrl(current.iconCode);
+  setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-fl-high", current.highFeelsLike);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-low", current.lowFeelsLike);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
+}
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" });
+const dailySection = document.querySelector("[data-day-section]");
+const dayCardTemplate = document.getElementById("day-card-template");
+
+function renderDailyWeather(daily) {
+  dailySection.innerHTML = "";
+  daily.forEach((day) => {
+    const element = dayCardTemplate.content.cloneNode(true);
+    setValue("temp", day.maxTemp, { parent: element });
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode);
+    dailySection.append(element);
+  });
+}
+
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric", hour12: true });
+const hourlySection = document.querySelector("[data-hour-section]");
+const hourRowTemplate = document.getElementById("hour-row-template");
+
+function renderHourlyWeather(hourly) {
+  hourlySection.innerHTML = "";
+  hourly.forEach((hour) => {
+    const element = hourRowTemplate.content.cloneNode(true);
+    setValue("temp", hour.temp, { parent: element });
+    setValue("fl-temp", hour.feelsLike, { parent: element });
+    setValue("wind", hour.windSpeed, { parent: element });
+    setValue("precip", hour.precip, { parent: element });
+    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element });
+    setValue("time", HOUR_FORMATTER.format(hour.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode);
+    hourlySection.append(element);
+  });
+}
+
+// DARKMODE
+
+document.getElementById("darkModeToggle").addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+  const iconDM = document.getElementById("iconDM");
+  if (document.body.classList.contains("dark-mode")) {
+    iconDM.classList.remove("fa-sun");
+    iconDM.classList.add("fa-moon");
+  } else {
+    iconDM.classList.remove("fa-moon");
+    iconDM.classList.add("fa-sun");
+  }
+}
